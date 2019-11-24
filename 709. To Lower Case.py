d1a21b1237f11747656991f0a711d7349e9bee2c
HM <zxjonathanzx@gmail.com>
2019-11-24 22:38:12 +0800

    2019/11/24

---
+++ 709. To Lower Case.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return str.lower()
+        
+#url: https://leetcode.com/problems/to-lower-case/
